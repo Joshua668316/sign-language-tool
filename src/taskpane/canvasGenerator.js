2b2a77eca342e1104668bb9b6a21862116649a93
Joshua Stalla <joshua.stalla@adesso.de>
2024-02-18 12:40:05 +0100

    outsource canvas config to struct

--- src/taskpane/canvasGenerator.js
+++ src/taskpane/canvasGenerator.js
@@ -1,26 +1,37 @@
 const { createCanvas } = require('canvas');
 
+function canvasConfig(imageSize, padding, textSpace, numPictures, img) {
+  this.imageSize = imageSize;
+  this.padding = padding;
+  this.textSpace = textSpace;
+  this.numPictures = numPictures;
+    
+  this.width = imageSize * numPictures + padding * (numPictures + 1);
+  this.height = imageSize + textSpace;
+    
+  this.scale = imageSize / Math.max(img.naturalWidth, img.naturalHeight);
+  this.imgWidth = this.scale * img.naturalWidth;
+  this.imgHeight = this.scale * img.naturalHeight;
+  this.dx = (this.imageSize - this.imgWidth) / 2;
+  this.dy = (this.imageSize - this.imgHeight) / 2;
+}
+
 export function createImageBase64(img, words) {
     const numPictures = words.length;
     const imageSize = 245
     const padding = 20;
     const textSpace = 80; 
-    const width = imageSize * numPictures + padding * (numPictures + 1);
-    const height = imageSize + textSpace
-    const canvas = createCanvas(width, height);
+
+    const conf = new canvasConfig(imageSize, padding, textSpace, numPictures, img);
+
+    const canvas = createCanvas(conf.width, conf.height);
     const ctx = canvas.getContext('2d');
-  
-    const scale = imageSize / Math.max(img.naturalWidth, img.naturalHeight);
-    const imgWidth = scale * img.naturalWidth;
-    const imgHeight = scale * img.naturalHeight;
-    const dx = (imageSize - imgWidth) / 2;
-    const dy = (imageSize - imgHeight) / 2;
   
     ctx.fillStyle = '#DDDDDD';
   
     for (let i = 1; i <= numPictures; i++) {
-      ctx.fillRect(padding * i + imageSize * (i - 1), padding, imageSize, imageSize);
-      ctx.drawImage(img, padding * i + imageSize * (i - 1) + dx, padding + dy, imgWidth, imgHeight);
+      ctx.fillRect(conf.padding * i + conf.imageSize * (i - 1), conf.padding, conf.imageSize, conf.imageSize);
+      ctx.drawImage(img, conf.padding * i + conf.imageSize * (i - 1) + conf.dx, conf.padding + conf.dy, conf.imgWidth, conf.imgHeight);
     }
   
     // Set text style
@@ -30,7 +41,7 @@ export function createImageBase64(img, words) {
     ctx.textBaseline = 'middle';
   
     for (let i = 1; i <= numPictures; i++) {
-      ctx.fillText(words[i - 1], padding * i + imageSize * (i - 0.5), 0.9 * canvas.height);
+      ctx.fillText(words[i - 1], conf.padding * i + conf.imageSize * (i - 0.5), 0.9 * canvas.height);
     }
   
     // Convert canvas to Base64 string (without the data:image/png;base64, prefix)
