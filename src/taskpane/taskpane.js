4e2900c1b4101fcceb5c032f18f0711676394128
Joshua Stalla <joshua.stalla@adesso.de>
2024-02-24 13:47:28 +0100

    cleanup

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global document, Office */
-
 import { createCanvasBase64 } from "./canvasGenerator";
 
 Office.onReady((info) => {
